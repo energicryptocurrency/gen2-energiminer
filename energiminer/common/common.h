4927f217fb3ec1e731e8114d4987af8137b91e2e
levongh <levon_ghukasyan@live.com>
2018-04-04 17:53:11 +0400

    removed unnacessary functions,  code cleanup

--- energiminer/common/common.h
+++ energiminer/common/common.h
@@ -194,16 +194,6 @@ namespace energi
     return _out;
   }
 
-  int varint_encode(unsigned char *p, uint64_t n);
-  void bin2hex(char *s, const unsigned char *p, size_t len);
-  char *abin2hex(const unsigned char *p, size_t len);
-  bool hex2bin(unsigned char *p, const char *hexstr, size_t len);
-  bool fulltest(const uint32_t *hash, const uint32_t *target);
-  extern "C" void sha256d(unsigned char *hash, const unsigned char *data, int len);
-
-
-  #define _ALIGN(x) __attribute__ ((aligned(x)))
-
   static inline bool is_windows(void) {
   #ifdef WIN32
     return 1;
@@ -232,64 +222,6 @@ namespace energi
   #include <sys/endian.h>
   #endif
 
-  typedef unsigned char uchar;
-
-  #if !HAVE_DECL_BE32DEC
-  static inline uint32_t be32dec(const void *pp)
-  {
-    const uint8_t *p = (uint8_t const *)pp;
-    return ((uint32_t)(p[3]) + ((uint32_t)(p[2]) << 8) +
-        ((uint32_t)(p[1]) << 16) + ((uint32_t)(p[0]) << 24));
-  }
-  #endif
-
-  #if !HAVE_DECL_LE32DEC
-  static inline uint32_t le32dec(const void *pp)
-  {
-    const uint8_t *p = (uint8_t const *)pp;
-    return ((uint32_t)(p[0]) + ((uint32_t)(p[1]) << 8) +
-        ((uint32_t)(p[2]) << 16) + ((uint32_t)(p[3]) << 24));
-  }
-  #endif
-
-  #if !HAVE_DECL_BE32ENC
-  static inline void be32enc(void *pp, uint32_t x)
-  {
-    uint8_t *p = (uint8_t *)pp;
-    p[3] = x & 0xff;
-    p[2] = (x >> 8) & 0xff;
-    p[1] = (x >> 16) & 0xff;
-    p[0] = (x >> 24) & 0xff;
-  }
-  #endif
-
-  #if !HAVE_DECL_LE32ENC
-  static inline void le32enc(void *pp, uint32_t x)
-  {
-    uint8_t *p = (uint8_t *)pp;
-    p[0] = x & 0xff;
-    p[1] = (x >> 8) & 0xff;
-    p[2] = (x >> 16) & 0xff;
-    p[3] = (x >> 24) & 0xff;
-  }
-  #endif
-
-  #if !HAVE_DECL_LE16DEC
-  static inline uint16_t le16dec(const void *pp)
-  {
-    const uint8_t *p = (uint8_t const *)pp;
-    return ((uint16_t)(p[0]) + ((uint16_t)(p[1]) << 8));
-  }
-  #endif
-
-  #if !HAVE_DECL_LE16ENC
-  static inline void le16enc(void *pp, uint16_t x)
-  {
-    uint8_t *p = (uint8_t *)pp;
-    p[0] = x & 0xff;
-    p[1] = (x >> 8) & 0xff;
-  }
-  #endif
 }
 
 
